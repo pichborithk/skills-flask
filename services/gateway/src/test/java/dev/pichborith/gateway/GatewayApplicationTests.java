23eff2c53dee37d416db820a0fbf752b42268cff
Pichborith Kong <pichborith@gmail.com>
2024-08-31 12:15:11 -0700

    add gateway service

---
+++ services/gateway/src/test/java/dev/pichborith/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pichborith.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
