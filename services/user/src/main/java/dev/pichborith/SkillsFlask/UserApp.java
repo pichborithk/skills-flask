9b078c223326b47a42eb776e4d8f30d2b2b66bd0
Pichborith Kong <pichborith@gmail.com>
2024-08-31 16:04:47 -0700

    rename user app

--- services/user/src/main/java/dev/pichborith/SkillsFlask/UserApp.java
+++ services/user/src/main/java/dev/pichborith/SkillsFlask/UserApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserApplication {
+public class UserApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication.run(UserApp.class, args);
 	}
 
 }
