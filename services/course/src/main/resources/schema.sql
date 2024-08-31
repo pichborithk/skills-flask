9b82bde814f3d4179d370b1a7bbd04cd3a5ce10d
Pichborith Kong <pichborith@gmail.com>
2024-08-31 16:03:45 -0700

    schema for course

---
+++ services/course/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS courses;
+
+
+CREATE TABLE courses (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL UNIQUE,
+    price DECIMAL(10, 2)
+    author_id VARCHAR(255) NOT NUll
+)
