5f08ea8233ad86c95795316a65d913157505f877
Pichborith Kong <pichborith@gmail.com>
2024-08-31 12:14:25 -0700

    add discovery service

---
+++ services/discovery/src/test/java/dev/pichborith/discovery/DiscoveryAppTests.java
@@ -0,0 +1,13 @@
+package dev.pichborith.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
