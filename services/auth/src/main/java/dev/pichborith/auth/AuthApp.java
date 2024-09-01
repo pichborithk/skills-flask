81424af3270799b2838766a857c4b384994a6c3c
Pichborith Kong <pichborith@gmail.com>
2024-08-31 22:48:24 -0700

    rename auth app

--- services/auth/src/main/java/dev/pichborith/auth/AuthApp.java
+++ services/auth/src/main/java/dev/pichborith/auth/AuthApp.java
@@ -2,12 +2,14 @@ package dev.pichborith.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class AuthApplication {
+@EnableFeignClients
+public class AuthApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
+		SpringApplication.run(AuthApp.class, args);
 	}
 
 }
