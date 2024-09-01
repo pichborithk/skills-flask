9c485d4aec0a12e04aa14fc1ccdffdb3c9bac608
Pichborith Kong <pichborith@gmail.com>
2024-08-31 18:56:34 -0700

    rename gateway app

--- services/gateway/src/main/java/dev/pichborith/gateway/GatewayApp.java
+++ services/gateway/src/main/java/dev/pichborith/gateway/GatewayApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class GatewayApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(GatewayApp.class, args);
 	}
 
 }
