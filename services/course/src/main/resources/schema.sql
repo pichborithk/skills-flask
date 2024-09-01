1c4cd852ebacf0aa095805d454557878b1942124
Pichborith Kong <pichborith@gmail.com>
2024-08-31 20:32:07 -0700

    update config for courser service

--- services/course/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS courses;
-
-
-CREATE TABLE courses (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL UNIQUE,
-    price DECIMAL(10, 2),
-    author_id VARCHAR(255) NOT NUll
-)
