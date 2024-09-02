2a2f614977d9912821a10f8d6d62b0007aa8e60c
Pichborith Kong <pichborith@gmail.com>
2024-09-02 15:51:54 -0700

    set up security

---
+++ services/user/src/main/java/dev/pichborith/SkillsFlask/configs/JwtAuthEntryPoint.java
@@ -0,0 +1,31 @@
+package dev.pichborith.SkillsFlask.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.pichborith.SkillsFlask.exceptions.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        var responseBody = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(),
+                                             authException.getMessage());
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getWriter(), responseBody);
+    }
+}
---
+++ services/user/src/main/java/dev/pichborith/SkillsFlask/configs/SecurityConfig.java
@@ -0,0 +1,42 @@
+package dev.pichborith.SkillsFlask.configs;
+
+import org.springframework.http.HttpMethod;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(
+                req -> req.requestMatchers(HttpMethod.POST, "/api/users/register")
+                          .permitAll()
+                          .requestMatchers(HttpMethod.POST, "/api/users/login")
+                          .permitAll()
+                          .anyRequest().authenticated()
+            )
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(STATELESS))
+            .exceptionHandling(
+                ex -> ex.authenticationEntryPoint(jwtAuthEntryPoint))
+            .addFilterBefore(jwtAuthFilter,
+                             UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
