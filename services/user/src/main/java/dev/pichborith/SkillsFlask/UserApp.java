170251a6959d3a7267a399b5b8dcd0136867e475
Pichborith Kong <pichborith@gmail.com>
2024-09-02 15:49:04 -0700

    enable feign clients

--- services/user/src/main/java/dev/pichborith/SkillsFlask/UserApp.java
+++ services/user/src/main/java/dev/pichborith/SkillsFlask/UserApp.java
@@ -2,8 +2,10 @@ package dev.pichborith.SkillsFlask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UserApp {
 
 	public static void main(String[] args) {
