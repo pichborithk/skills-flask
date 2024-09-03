e207a1cc0bab35051a3d4a75444c9d8eaa16adea
Pichborith Kong <pichborith@gmail.com>
2024-09-03 12:22:50 -0700

    initialize api

---
+++ api/src/main/java/dev/pichborith/SkillsLabAcademyAPI/SkillsLabAcademyApi.java
@@ -0,0 +1,13 @@
+package dev.pichborith.SkillsLabAcademyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsLabAcademyApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsLabAcademyApi.class, args);
+	}
+
+}
---
+++ api/src/test/java/dev/pichborith/SkillsLabAcademyAPI/SkillsLabAcademyApiTests.java
@@ -0,0 +1,13 @@
+package dev.pichborith.SkillsLabAcademyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsLabAcademyApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
