6b199d643fee4a7d26d02b63387b5d8f36aec787
Pichborith Kong <pichborith@gmail.com>
2024-09-05 16:41:29 -0700

    config color and font in tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gilroy: ['Gilroy-ExtraBold', 'serif'],
+        gilroyLight: ['Gilroy-Light', 'serif'],
+        jura: ['Jura', 'san-serif'],
+      },
+      colors: {
+        // primary: { DEFAULT: '#eb455f' },
+        // secondary: { DEFAULT: '#fcffe7' },
+        checked: { DEFAULT: '#1bca00' },
+        primary: { DEFAULT: '#F652A0' },
+        secondary: { DEFAULT: '#36eee0' },
+        status: { success: '#00B087', error: '#FFC5C5' },
+      },
+      boxShadow: {
+        full: '0px 1px 6px 1px rgba(0, 0, 0, 0.4)',
+      },
+    },
   },
   plugins: [],
 };
