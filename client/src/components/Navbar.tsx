fe4926dd9e9f9855117d8a9769126a07d516ead1
Pichborith Kong <pichborith@gmail.com>
2024-09-05 19:44:06 -0700

    adjust style on navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,15 +5,15 @@ import logo from '../assets/images/Skills_Lab.png';
 const Navbar = () => {
   return (
     <header className='absolute top-0 w-full bg-slate-50 shadow-sm'>
-      <section className='mx-auto flex max-w-7xl items-center px-8 py-2'>
+      <section className='mx-auto flex max-w-7xl items-center py-2'>
         <Link to='/'>
           <img src={logo} alt='logo web site' className='max-h-24' />
         </Link>
-        <nav className='text-primary relative flex grow items-center justify-between gap-6 px-8 pt-4 text-xl font-thin'>
+        <nav className='relative flex grow items-center justify-between gap-6 px-8 pt-4 text-xl font-thin text-primary'>
           <div>
             <Link
               to='/courses'
-              className='hover:border-secondary border-b-4 border-transparent px-2'
+              className='border-b-4 border-transparent px-2 hover:border-secondary'
             >
               Our Classes
             </Link>
@@ -21,13 +21,13 @@ const Navbar = () => {
           <div className='flex items-center gap-8'>
             <Link
               to='/login'
-              className='hover:border-secondary border-b-4 border-transparent px-2'
+              className='border-b-4 border-transparent px-2 hover:border-secondary'
             >
               Sign In
             </Link>
             <Link
               to='/register'
-              className='bg-primary border-primary hover:text-primary rounded-md border-2 px-8 py-1 text-slate-50 hover:bg-slate-50'
+              className='rounded-md border-2 border-primary bg-primary px-8 py-1 text-slate-50 hover:bg-slate-50 hover:text-primary'
             >
               Become a Member
             </Link>
