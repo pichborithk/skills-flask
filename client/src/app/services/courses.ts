2b3bb8fcf472c9b70a83fcc740826a7dfbb6899f
Pichborith Kong <pichborith@gmail.com>
2024-09-06 13:10:45 -0700

    add course slice RTK query

--- client/src/app/services/courses.ts
+++ client/src/app/services/courses.ts
@@ -1,14 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { Course } from '../../types/course.types';
+import { CourseResponse } from '../../types/course.types';
 
-export const coursesApi = createApi({
+export const courseSlice = createApi({
   reducerPath: 'courses',
   baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_API_URL}` }),
   endpoints: builder => ({
-    getCourses: builder.query<Course[], void>({
+    getCourses: builder.query<CourseResponse[], void>({
       query: () => 'courses',
     }),
   }),
 });
 
-export const { useGetCoursesQuery } = coursesApi;
+export const { useGetCoursesQuery } = courseSlice;
