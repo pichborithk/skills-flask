bf8a722f4bc76ee5a85bc3fffe5d6f77307c7511
Pichborith Kong <pichborith@gmail.com>
2024-09-06 14:14:47 -0700

    update navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
 
 import logo from '../assets/images/Skills_Lab.png';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { tokenClear } from '../app/reducers';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const { token } = useAppSelector(state => state.auth);
+  const [isOpen, setIsOpen] = useState(false);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    dispatch(tokenClear());
+    setIsOpen(false);
+    navigate('/');
+  }
+
   return (
     <header className='absolute top-0 w-full bg-slate-50 shadow-sm'>
       <section className='mx-auto flex max-w-7xl items-center py-2'>
@@ -18,19 +33,41 @@ const Navbar = () => {
               Our Classes
             </Link>
           </div>
-          <div className='flex items-center gap-8'>
-            <Link
-              to='/login'
-              className='border-b-4 border-transparent px-2 hover:border-secondary'
-            >
-              Sign In
-            </Link>
-            <Link
-              to='/register'
-              className='rounded-md border-2 border-primary bg-primary px-8 py-1 text-slate-50 hover:bg-slate-50 hover:text-primary'
-            >
-              Become a Member
-            </Link>
+          <div className='relative flex items-center gap-8'>
+            {!token && (
+              <>
+                <Link
+                  to='/login'
+                  className='border-b-4 border-transparent px-2 hover:border-secondary'
+                >
+                  Sign In
+                </Link>
+                <Link
+                  to='/register'
+                  className='rounded-md border-2 border-primary bg-primary px-8 py-1 text-slate-50 hover:bg-slate-50 hover:text-primary'
+                >
+                  Become a Member
+                </Link>
+              </>
+            )}
+            {token && (
+              <FaUserCircle
+                size={40}
+                color='hotpink'
+                onClick={() => setIsOpen(!isOpen)}
+              />
+            )}
+            {token && isOpen && (
+              <ul className='text-md absolute right-0 top-10 rounded-md bg-primary bg-opacity-50 text-slate-50 [&>*]:px-4 [&>*]:py-2 hover:[&>*]:cursor-pointer hover:[&>*]:bg-primary'>
+                <li className='rounded-t-md' onClick={() => setIsOpen(false)}>
+                  <Link to='/dashboard'>Dashboard</Link>
+                </li>
+                <li>Setting</li>
+                <li onClick={handleLogout} className='rounded-b-md'>
+                  Sign Out
+                </li>
+              </ul>
+            )}
           </div>
         </nav>
       </section>
