e6b1332a9da8629d969ee2392e74f70e7ded984b
Pichborith Kong <pichborith@gmail.com>
2024-09-06 21:41:48 -0700

    login request handler

--- client/src/app/reducers/authSlice.ts
+++ client/src/app/reducers/authSlice.ts
@@ -27,7 +27,21 @@ export const register = createAsyncThunk(
     return await response.json();
   },
 );
-// export const login = createAsyncThunk('auth/login', loginUser);
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (request: Partial<UserRequest>): Promise<AuthResponse | void> => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(request),
+    });
+
+    return await response.json();
+  },
+);
 
 const authSlice = createSlice({
   name: 'auth',
@@ -60,6 +74,32 @@ const authSlice = createSlice({
         },
       )
       .addCase(register.rejected, state => {
+        state.isLoading = false;
+        state.token = '';
+        localStorage.removeItem('TOKEN');
+        toast.error('Something went wrong');
+      });
+
+    builder
+      .addCase(login.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(
+        login.fulfilled,
+        (state, action: PayloadAction<AuthResponse | void>) => {
+          state.isLoading = false;
+          if (action.payload) {
+            state.token = action.payload.token;
+            localStorage.setItem('TOKEN', action.payload.token);
+            toast.success('Login successful');
+          } else {
+            localStorage.removeItem('TOKEN');
+            toast.error('Something went wrong');
+            return initialState;
+          }
+        },
+      )
+      .addCase(login.rejected, state => {
         state.isLoading = false;
         state.token = '';
         localStorage.removeItem('TOKEN');
--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,52 +1,37 @@
-import { Link } from 'react-router-dom';
-import { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FormEvent, useEffect, useRef } from 'react';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { login } from '../app/reducers';
 
 const Login = () => {
+  const { token, isLoading } = useAppSelector(state => state.auth);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const usernameInput = useRef<HTMLInputElement>(null);
   const passwordInput = useRef<HTMLInputElement>(null);
 
-  // useEffect(() => {
-  //   if (token) {
-  //     localStorage.setItem('TOKEN', token);
-  //     setRoute('home');
-  //     navigate('/');
-  //     return;
-  //   }
-  // }, [token]);
+  useEffect(() => {
+    if (token) {
+      navigate('/');
+      return;
+    }
+  }, [token]);
 
-  // useEffect(() => {
-  //   setRoute('login');
-  // }, []);
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const username = usernameInput.current!.value;
+    const password = passwordInput.current!.value;
 
-  // async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-  //   event.preventDefault();
+    dispatch(login({ username, password }));
 
-  //   try {
-  //     const result = await userLogin(username, password);
-  //     if (result.error) {
-  //       toast.error(result.message);
-  //       return;
-  //     }
-
-  //     if (result.token) {
-  //       setToken(result.token);
-  //       localStorage.setItem('TOKEN', result.token);
-  //       setRoute('home');
-  //       navigate('/');
-  //       toast.success('Logged In');
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //   } finally {
-  //     setUsername('');
-  //     setPassword('');
-  //   }
-  // }
+    usernameInput.current!.value = '';
+    passwordInput.current!.value = '';
+  }
 
   return (
     <div className='section-min-height mx-auto mb-8 flex max-w-7xl flex-col items-center justify-center gap-4 px-2'>
       <form
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit}
         className='relative flex w-1/2 flex-col items-center justify-evenly gap-8 rounded-2xl border px-20 py-24 text-xl text-primary shadow-2xl'
       >
         <h1 className='text-4xl font-bold text-primary'>Sign In</h1>
@@ -78,7 +63,10 @@ const Login = () => {
           />
         </fieldset>
         <div className='mt-4 w-full text-center'>
-          <button className='mb-8 w-full rounded-lg border-2 border-primary px-4 py-2 font-semibold hover:bg-primary hover:text-slate-50'>
+          <button
+            className='mb-8 w-full rounded-lg border-2 border-primary px-4 py-2 font-semibold hover:bg-primary hover:text-slate-50'
+            disabled={isLoading}
+          >
             Sign In
           </button>
           <p className='text-base'>
