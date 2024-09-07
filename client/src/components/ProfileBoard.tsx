dfd3afcae3bec27b9f06383673b0a5d37adc187f
Pichborith Kong <pichborith@gmail.com>
2024-09-07 00:54:06 -0700

    refactor code

--- client/src/components/ProfileBoard.tsx
+++ client/src/components/ProfileBoard.tsx
@@ -1,22 +1,21 @@
 import { FaUserCircle } from 'react-icons/fa';
-import { useGetUserQuery } from '../app/services';
-import { useAppSelector } from '../app/hooks';
+import { useOutletContext } from 'react-router-dom';
+import { DashboardContext } from '../types/Dashboard.types';
 
 const ProfileBoard = () => {
-  const { token } = useAppSelector(state => state.auth);
-  const { data: user } = useGetUserQuery(token);
+  const { user } = useOutletContext<DashboardContext>();
 
   return (
-    <div className='flex min-h-full grow flex-col items-center gap-8 rounded-md bg-slate-50 py-8'>
+    <div className='flex min-h-full grow flex-col items-center gap-8 rounded-md bg-slate-50 py-16 shadow-sm'>
       <FaUserCircle size={120} color='hotpink' />
       <div className='flex flex-col items-center gap-4 text-xl font-medium'>
         <div className='grid w-full grid-cols-2 gap-4'>
           <p className='text-right'>Username:</p>
-          <p className='border-b-2'>{user?.username}</p>
+          <p className='border-b-2'>{user.username}</p>
         </div>
         <div className='grid w-full grow grid-cols-2 gap-4'>
           <p className='text-right'>Role:</p>
-          <p className='border-b-2'>{user?.role}</p>
+          <p className='border-b-2'>{user.role}</p>
         </div>
       </div>
     </div>
